871318756e5ddfa378a2dad55ae537ffed710426
Bryan Neves <bryan.neves@indt.org.br>
2025-05-25 00:46:49 -0400

    fix: adjust welcome project title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-data-hub' title`, () => {
+  it(`should have the 'the-dog-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-data-hub');
+    expect(app.title).toEqual('the-dog-frontend');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-data-hub'
+      'Hello, the-dog-frontend'
     );
   });
 });
