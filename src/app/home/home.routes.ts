8aa82c0f8e90dddc5024a9337e6176a2af6fdb91
Bryan Neves <bryan.neves@indt.org.br>
2025-05-25 00:29:44 -0400

    fix: adjust welcome route

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { userRoute } from './user/user.routes';
+import { welcomeRoute } from './welcome/welcome.routes';
 
 export const homeRoute: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
+      welcomeRoute,
       userRoute,
       {
         path: '',
